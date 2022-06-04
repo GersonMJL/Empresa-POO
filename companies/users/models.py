ab495fc2890761fc3b24a06f144eb4e3221acf44
Gerson Jatobá <gersonmjl@gmail.com>
2022-06-04 20:05:54 -0300

    UserCreationForm

--- companies/users/models.py
+++ companies/users/models.py
@@ -1,78 +1,85 @@
 import validate_cpf
 from django.db import models
-from django.contrib.auth.models import (
-    BaseUserManager,
-    AbstractBaseUser,
-    PermissionsMixin,
-)
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, name, password=None, groups=None):
+    def create_user(self, email, nome, password=None):
+        """Cria um usuário com email, nome e senha.
+        Args:
+            email (str): Email do usuário.
+            nome (str): Nome do usuário.
+            password (str, optional): Senha do usuário. Defaults to None.
+        Raises:
+            ValueError: Levanta exceção caso o email seja vazio.
+            ValueError: Levanta exceção caso o nome seja vazio.
+        Returns:
+            Usuario: Objeto de usuário criado.
         """
-        Creates and saves a User with the given email and password.
-        """
+
         if not email:
-            raise ValueError("Usuários precisam de email")
-        if not name:
-            raise ValueError("Usuários precisam de nome")
+            raise ValueError("Usuários precisam de um email.")
+        if not nome:
+            raise ValueError("Usuários precisam de um nome.")
 
-        user = self.model(
-            email=self.normalize_email(email),
-            name=name,
-        )
-        if groups:
-            for group in groups:
-                user.groups.add(group)
+        usuario = self.model(email=self.normalize_email(email), nome=nome)
+        usuario.set_password(password)
+        usuario.save(using=self._db)
 
-    def create_superuser(self, email, password):
+        return usuario
+
+    def create_superuser(self, email, nome, password=None):
+        """Cria um superusuário com email, nome e senha.
+        Args:
+            email (str): Email do usuário.
+            nome (str): Nome do usuário.
+            password (str, optional): Senha do usuário. Defaults to None.
+        Returns:
+            Usuario: Objeto de usuário criado.
         """
-        Creates and saves a superuser with the given email and password.
-        """
-        user = self.create_user(
-            email,
-            password=password,
-        )
-        user.is_admin = True
-        user.save(using=self._db)
-        return user
+
+        usuario = self.create_user(email, nome=nome, password=password)
+        usuario.is_active = True
+        usuario.is_admin = True
+        usuario.is_superuser = True
+        usuario.save(using=self._db)
+        return usuario
 
 
-class User(AbstractBaseUser, PermissionsMixin):
+class User(AbstractUser):
+    DEPARTMENT_MANAGER = 1
+    COMPANY_MANAGER = 2
+    EMPLOYEE = 3
+
+    ROLE_CHOICES = (
+        (DEPARTMENT_MANAGER, "Gerente de Departamento"),
+        (COMPANY_MANAGER, "Gerente de Empresa"),
+        (EMPLOYEE, "Funcionário"),
+    )
+
+    role = models.PositiveSmallIntegerField(
+        choices=ROLE_CHOICES, blank=True, null=True, verbose_name="Cargo"
+    )
     cpf = models.CharField(
         verbose_name="CPF", primary_key=True, unique=True, max_length=11
     )
     email = models.EmailField(verbose_name="Email", max_length=100, unique=True)
     name = models.CharField(verbose_name="Nome", max_length=100)
-    groups = models.ManyToManyField("Group", verbose_name="Grupos", blank=True)
     is_active = models.BooleanField(verbose_name="Ativo", default=True)
     is_admin = models.BooleanField(verbose_name="Administrador", default=False)
 
-    objects = UserManager()
-
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["name"]
+    REQUIRED_FIELDS = ["name", "cpf"]
 
     def __str__(self):
-        return self.email
+        return self.name
 
-    def has_perm(self, perm, obj=None):
-        "Does the user have a specific permission?"
-        # Simplest possible answer: Yes, always
-        return True
-
-    def has_module_perms(self, app_label):
-        "Does the user have permissions to view the app `app_label`?"
-        # Simplest possible answer: Yes, always
-        return True
-
-    def cpf_validate(self):
+    def validate_cpf(self):
         if validate_cpf.validate_cpf(self.cpf):
             return True
         return False
 
-    @property
-    def is_staff(self):
-        "Is the user a member of staff?"
-        # Simplest possible answer: All admins are staff
-        return self.is_admin
+    def save(self, *args, **kwargs):
+        if not self.validate_cpf():
+            raise ValueError("CPF inválido")
+        super(User, self).save(*args, **kwargs)
