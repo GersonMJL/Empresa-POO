1a7b88b33dd02cb8d1d6738ab90f67d5df408f8e
Gerson Jatobá <gersonmjl@gmail.com>
2022-06-06 19:34:52 -0300

    Fixed user model

--- companies/enterprisems/models.py
+++ companies/enterprisems/models.py
@@ -1,15 +1,7 @@
 from django.db import models
-from django.core.exceptions import ValidationError
-import validate_cpf
 
-"""
-Tipos de pessoas que podem ser cadastradas no sistema:
-"""
-ROLES_CHOICES = (
-    ("1", "Gerente de Departamento"),
-    ("2", "Gerente de Empresa"),
-    ("3", "Funcionário"),
-)
+# from django.core.exceptions import ValidationError
+# import validate_cpf
 
 
 class Department(models.Model):
@@ -40,56 +32,6 @@ class Company(models.Model):
     code_id = models.AutoField(primary_key=True, verbose_name="Código", unique=True)
     name = models.CharField(verbose_name="Nome", max_length=100)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
-
-
-class Person(models.Model):
-    cpf = models.CharField(
-        verbose_name="CPF", primary_key=True, unique=True, max_length=11
-    )
-    role = models.CharField(verbose_name="Cargo", choices=ROLES_CHOICES, max_length=100)
-    allowance = models.BooleanField(verbose_name="Abono", default=False)
-    name = models.CharField(verbose_name="Nome", max_length=100)
-    telephone = models.IntegerField(verbose_name="Telefone", max_length=11)
-    email = models.EmailField(verbose_name="E-mail", max_length=100)
-    street = models.CharField(verbose_name="Rua", max_length=100)
-    city = models.CharField(verbose_name="Cidade", max_length=100)
-    state = models.CharField(verbose_name="Estado", max_length=100)
-    wage = models.DecimalField(verbose_name="Salário", decimal_places=2, max_digits=10)
-
-    def __str__(self):
-        return self.name
-
-    def validate_cpf(self):
-        if validate_cpf.validate_cpf(self.cpf):
-            return True
-        return False
-
-    def save(self, *args, **kwargs):
-        if self.validate_cpf():
-            super(self).save(*args, **kwargs)
-        else:
-            raise ValidationError("CPF inválido")
-
-
-class Employee(Person):
-    company = models.ForeignKey(Company, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
-
-
-class DepartmentManager(Person):
-    department = models.ForeignKey(Department, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
-
-
-class CompanyManager(Person):
-    company = models.ForeignKey(Company, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
--- companies/users/models.py
+++ companies/users/models.py
@@ -1,10 +1,11 @@
 import validate_cpf
+import uuid
 from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, nome, password=None):
+    def create_user(self, email, name, cpf, password=None, **extra_fields):
         """Cria um usuário com email, nome e senha.
         Args:
             email (str): Email do usuário.
@@ -19,34 +20,41 @@ class UserManager(BaseUserManager):
 
         if not email:
             raise ValueError("Usuários precisam de um email.")
-        if not nome:
+        if not name:
             raise ValueError("Usuários precisam de um nome.")
 
-        usuario = self.model(email=self.normalize_email(email), nome=nome)
-        usuario.set_password(password)
-        usuario.save(using=self._db)
+        user = self.model(
+            email=self.normalize_email(email), name=name, cpf=cpf, **extra_fields
+        )
+        user.set_password(password)
+        user.save()
 
-        return usuario
+        return user
 
-    def create_superuser(self, email, nome, password=None):
+    def create_superuser(self, email, name, password, **extra_fields):
         """Cria um superusuário com email, nome e senha.
+
         Args:
             email (str): Email do usuário.
             nome (str): Nome do usuário.
             password (str, optional): Senha do usuário. Defaults to None.
+
         Returns:
             Usuario: Objeto de usuário criado.
         """
 
-        usuario = self.create_user(email, nome=nome, password=password)
-        usuario.is_active = True
-        usuario.is_admin = True
-        usuario.is_superuser = True
-        usuario.save(using=self._db)
-        return usuario
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(("Superuser must have is_staff=True"))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(("Superuser must have is_superuser=True"))
+
+        return self.create_user(email, name, password, **extra_fields)
 
 
-class User(AbstractUser):
+class User(AbstractUser, PermissionsMixin):
     DEPARTMENT_MANAGER = 1
     COMPANY_MANAGER = 2
     EMPLOYEE = 3
@@ -57,29 +65,35 @@ class User(AbstractUser):
         (EMPLOYEE, "Funcionário"),
     )
 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(verbose_name="Email", max_length=100, unique=True)
+    cpf = models.CharField(
+        verbose_name="CPF",
+        unique=True,
+        max_length=11,
+        blank=True,
+        null=True,
+    )
     role = models.PositiveSmallIntegerField(
         choices=ROLE_CHOICES, blank=True, null=True, verbose_name="Cargo"
     )
-    cpf = models.CharField(
-        verbose_name="CPF", primary_key=True, unique=True, max_length=11
-    )
-    email = models.EmailField(verbose_name="Email", max_length=100, unique=True)
     name = models.CharField(verbose_name="Nome", max_length=100)
     is_active = models.BooleanField(verbose_name="Ativo", default=True)
     is_admin = models.BooleanField(verbose_name="Administrador", default=False)
 
+    objects = UserManager()
+
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["name", "cpf"]
+    REQUIRED_FIELDS = ["name"]
 
     def __str__(self):
         return self.name
 
     def validate_cpf(self):
-        if validate_cpf.validate_cpf(self.cpf):
-            return True
-        return False
+        return validate_cpf.validate_cpf(self.cpf)
 
     def save(self, *args, **kwargs):
-        if not self.validate_cpf():
-            raise ValueError("CPF inválido")
+        if self.cpf:
+            if not self.validate_cpf():
+                raise ValueError("CPF inválido")
         super(User, self).save(*args, **kwargs)
